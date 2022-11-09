8b3621688b9b79f135879734a3834cb10fb06258
Georgiy <4468618@gmail.com>
2022-11-09 12:27:00 +0200

    Create getExcelCellValue.php
    Add code to repo

---
+++ getExcelCellValue.php
@@ -0,0 +1,55 @@
+<?php
+require __DIR__.'/vendor/autoload.php';
+$inputFileName = './data/data.xlsx';
+	
+
+	/** Input file name, Excel file format (xslx), max rows in Excel file **/
+	
+$sheetname = 'TDSheet'; 
+	$inputFileType = 'Excel2007'; 
+  $max_count_value = 100;
+	
+/**  Create a new Reader of the type defined in $inputFileType  **/
+	$objReader = PHPExcel_IOFactory::createReader($inputFileType);
+
+	/**  Set which WorkSheets we want to load  **/ 
+	$objReader->setLoadSheetsOnly($sheetname); 
+
+	/**  Load $inputFileName to a PHPExcel Object  **/
+	$objPHPExcel = $objReader->load($inputFileName);
+
+
+/* Find something by first row field */
+	function findByBarCode ($barcode,$objPHPExcel) {
+	
+		$findString = $barcode;
+
+	
+		while($i < $max_count_value) {
+			
+			$current_string = $objPHPExcel->getActiveSheet()->getCell('A' . $i)->getValue();
+			
+			if ($current_string == $findString) {
+				$result = 'Bar Code ' . $objPHPExcel->getActiveSheet()->getCell('A' . $i)->getValue();
+				$result .= ' Name: ' . $objPHPExcel->getActiveSheet()->getCell('B' . $i)->getValue();
+				$result .= ' Price: (rozn) ' . $objPHPExcel->getActiveSheet()->getCell('C' . $i)->getValue();
+	
+			}
+			
+		}
+		
+		if (null==$result)	$result = 'Позиция не найдена ('; 
+		
+		return $result;
+	
+	
+	}
+	
+/* findByBarCode() - find by string in excel file function
+  @params:
+  findString (string) - string to find
+  objPHPExcel (object) - PHP Office Object
+*/
+	$resultToTelegram = findByBarCode($findString, $objPHPExcel);
+
+?>
